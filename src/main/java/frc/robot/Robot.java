824af196448af09e37d1069b7302be0c049c961d
turbojax07 <turbojax07@gmail.com>
2025-01-17 00:29:34 -0500

    Expanding single-line if statements.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
@@ -24,8 +20,13 @@ public class Robot extends LoggedRobot {
 
         Logger.addDataReceiver(new NT4Publisher());
 
-        if (isReal()) Logger.addDataReceiver(new WPILOGWriter("Log"));
-        if (isSimulation() && Constants.isReplay) Logger.setReplaySource(new WPILOGReader("replay.wpilog"));
+        if (isReal()) {
+            Logger.addDataReceiver(new WPILOGWriter("Log"));
+        }
+
+        if (isSimulation() && Constants.isReplay) {
+            Logger.setReplaySource(new WPILOGReader("replay.wpilog"));
+        }
 
         Logger.start();
     }
