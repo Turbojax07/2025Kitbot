19b91389e49a421af1c8db6e63477e1642693dc8
turbojax07 <turbojax07@gmail.com>
2025-01-17 00:55:33 -0500

    Expanding one-line declarations and adding comments

--- src/main/java/frc/robot/Subsystems/Gyro/GyroIO.java
+++ src/main/java/frc/robot/Subsystems/Gyro/GyroIO.java
@@ -6,21 +6,34 @@ import edu.wpi.first.units.measure.LinearAcceleration;
 import org.littletonrobotics.junction.AutoLog;
 
 public interface GyroIO {
+    /** The values to log through AdvantageKit. */
     @AutoLog
     public class GyroIOInputs{
-        Angle roll, pitch, yaw;
-        LinearAcceleration x_accel, y_accel, z_accel;
+        Angle roll;
+        Angle pitch;
+        Angle yaw;
+        LinearAcceleration x_accel;
+        LinearAcceleration y_accel;
+        LinearAcceleration z_accel;
     }
 
+    /** Updates the values of the inputs defined in the GyroIO interface. */
     public void updateInputs();
 
+    /** Gets the heading (yaw) of the robot as a {@link Rotation2d}. */
     public Rotation2d getHeading();
 
+    /** Gets the roll of the robot as an {@link Angle}. */
     public Angle getRoll();
+    /** Gets the pitch of the robot as an {@link Angle}. */
     public Angle getPitch();
+    /** Gets the yaw of the robot as an {@link Angle}. */
     public Angle getYaw();
 
+    /** Gets the acceleration of the robot along the x axis as an {@link LinearAcceleration}. */
     public LinearAcceleration getXAcceleration();
+    /** Gets the acceleration of the robot along the y axis as an {@link LinearAcceleration}. */
     public LinearAcceleration getYAcceleration();
+    /** Gets the acceleration of the robot along the z axis as an {@link LinearAcceleration}. */
     public LinearAcceleration getZAcceleration();
 }
