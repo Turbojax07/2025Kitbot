0458832946bd59f3c3964bf4e78ec1adaa269299
turbojax07 <turbojax07@gmail.com>
2025-01-17 00:54:07 -0500

    Finishing the SparkMax implementation of the Roller

--- src/main/java/frc/robot/Subsystems/Roller/RollerIOSparkMax.java
+++ src/main/java/frc/robot/Subsystems/Roller/RollerIOSparkMax.java
@@ -1,53 +1,70 @@
 package frc.robot.Subsystems.Roller;
 
+import static edu.wpi.first.units.Units.*;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 import edu.wpi.first.units.measure.Current;
 import edu.wpi.first.units.measure.Dimensionless;
 import edu.wpi.first.units.measure.Temperature;
 import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj.RobotController;
 import frc.robot.Constants.ControlMode;
+import frc.robot.Constants.RobotMap;
+import org.littletonrobotics.junction.Logger;
 
 public class RollerIOSparkMax implements RollerIO {
+    private SparkMax roller;
+    
+    private RollerIOInputsAutoLogged inputs;
+
+    /** Creates a new instance of RollerIOSparkMax. */
+    public RollerIOSparkMax() {
+        roller = new SparkMax(RobotMap.ARM_RollerId, MotorType.kBrushless);
+
+        inputs = new RollerIOInputsAutoLogged();
+    }
 
     @Override
     public void updateInputs() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updateInputs'");
+        inputs.current = getCurrent();
+        inputs.percent = getPercent();
+        inputs.temperature = getTemperature();
+        inputs.voltage = getVoltage();
+
+        Logger.processInputs("Roller_SparkMax", inputs);
     }
 
     @Override
     public void setSpeed(ControlMode mode, double magnitude) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setSpeed'");
+        inputs.mode = mode;
+        if (mode.equals(ControlMode.Voltage)) magnitude /= RobotController.getInputVoltage();
+
+        roller.set(magnitude);
     }
 
     @Override
     public Current getCurrent() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getCurrent'");
+        return Amps.of(roller.getOutputCurrent());
     }
 
     @Override
     public Dimensionless getPercent() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getPercent'");
+        return Percent.of(roller.getAppliedOutput());
     }
 
     @Override
     public Temperature getTemperature() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getTemperature'");
+        return Celsius.of(roller.getMotorTemperature());
     }
 
     @Override
     public Voltage getVoltage() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getVoltage'");
+        return Volts.of(roller.getBusVoltage()).times(getPercent());
     }
 
     @Override
     public ControlMode getMode() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getMode'");
+        return inputs.mode;
     }
-    
 }
